bd5b5c24739cfc0c6edaa482fd010a9635f72e98
NoF0rte <nof0rte@users.noreply.github.com>
2023-02-24 10:33:01 -0700

    Can specify the client IP and user agent when tracking opens and clicks

--- phish/phish.go
+++ phish/phish.go
@@ -7,6 +7,24 @@ import (
 	"net/url"
 )
 
+type TrackOption func(*trackOptions)
+type trackOptions struct {
+	userAgent string
+	clientIP  string
+}
+
+func WithUserAgent(userAgent string) TrackOption {
+	return func(to *trackOptions) {
+		to.userAgent = userAgent
+	}
+}
+
+func WithClientIP(ip string) TrackOption {
+	return func(to *trackOptions) {
+		to.clientIP = ip
+	}
+}
+
 type Client struct {
 	url    string
 	client *http.Client
@@ -25,20 +43,56 @@ func NewClient(url string) *Client {
 	}
 }
 
-func (c *Client) TrackOpen(rid string) (*http.Response, error) {
+func (c *Client) TrackOpen(rid string, opts ...TrackOption) (*http.Response, error) {
+	options := &trackOptions{}
+	for _, opt := range opts {
+		opt(options)
+	}
+
 	u, err := url.JoinPath(c.url, fmt.Sprintf("track?rid=%s", rid))
 	if err != nil {
 		return nil, err
 	}
 
-	return c.client.Get(u)
+	req, err := http.NewRequest(http.MethodGet, u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if options.userAgent != "" {
+		req.Header.Set("User-Agent", options.userAgent)
+	}
+
+	if options.clientIP != "" {
+		req.Header.Set("X-Forwarded-For", options.clientIP)
+	}
+
+	return c.client.Do(req)
 }
 
-func (c *Client) TrackClick(rid string) (*http.Response, error) {
+func (c *Client) TrackClick(rid string, opts ...TrackOption) (*http.Response, error) {
+	options := &trackOptions{}
+	for _, opt := range opts {
+		opt(options)
+	}
+
 	u, err := url.JoinPath(c.url, fmt.Sprintf("?rid=%s", rid))
 	if err != nil {
 		return nil, err
 	}
 
-	return c.client.Get(u)
+	req, err := http.NewRequest(http.MethodGet, u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if options.userAgent != "" {
+		req.Header.Set("User-Agent", options.userAgent)
+	}
+
+	if options.clientIP != "" {
+		req.Header.Set("X-Forwarded-For", options.clientIP)
+	}
+
+	return c.client.Do(req)
 }
