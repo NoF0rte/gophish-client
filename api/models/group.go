50b3776cf90e1493c85d4b55a741cde2b05b98dd
NoF0rte <nof0rte@users.noreply.github.com>
2023-04-25 09:56:35 -0600

    Create group file from

--- api/models/group.go
+++ api/models/group.go
@@ -2,15 +2,22 @@ package models
 
 import (
 	"encoding/json"
+	"os"
+	"path/filepath"
 	"time"
+
+	"github.com/gocarina/gocsv"
+	"gopkg.in/yaml.v2"
 )
 
 type Group struct {
-	ID           int64     `json:"id" yaml:"id"`
+	ID           int64     `json:"id" yaml:"-"`
 	Name         string    `json:"name" yaml:"name"`
 	NumTargets   int64     `json:"num_targets,omitempty" yaml:"-"`
-	Targets      []Target  `json:"targets,omitempty" yaml:"targets"`
+	Targets      []*Target `json:"targets,omitempty" yaml:"targets"`
+	TargetsFile  string    `json:"-" yaml:"targets-file"`
 	ModifiedDate time.Time `json:"modified_date" yaml:"-"`
+	varsReplaced bool
 }
 
 func (g *Group) ToJSON() (string, error) {
@@ -22,11 +29,120 @@ func (g *Group) ToJSON() (string, error) {
 	return string(data), nil
 }
 
+func (g *Group) replaceVars(vars map[string]string) error {
+	if g.varsReplaced {
+		return nil
+	}
+
+	name, err := templateReplace(g.Name, vars)
+	if err != nil {
+		return err
+	}
+	g.Name = name
+
+	for _, target := range g.Targets {
+		err = target.replaceVars(vars)
+		if err != nil {
+			return err
+		}
+	}
+
+	g.varsReplaced = true
+	return nil
+}
+
+func GroupFromFile(file string, vars map[string]string) (*Group, error) {
+	bytes, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	var group Group
+	err = yaml.Unmarshal(bytes, &group)
+	if err != nil {
+		return nil, err
+	}
+
+	parentDir := filepath.Dir(file)
+	if len(group.Targets) == 0 && group.TargetsFile != "" {
+		targetsPath := filepath.Join(parentDir, group.TargetsFile)
+
+		var targets []*Target
+		if filepath.Ext(targetsPath) == ".csv" {
+			targetsFile, err := os.Open(targetsPath)
+			if err != nil {
+				return nil, err
+			}
+			defer targetsFile.Close()
+
+			err = gocsv.Unmarshal(targetsFile, &targets)
+			if err != nil {
+				return nil, err
+			}
+
+			if len(targets) == 0 {
+				targetsFile.Seek(0, 0)
+				err = gocsv.UnmarshalWithoutHeaders(targetsFile, &targets)
+				if err != nil {
+					return nil, err
+				}
+			}
+		} else {
+			bytes, err := os.ReadFile(targetsPath)
+			if err != nil {
+				return nil, err
+			}
+
+			err = yaml.Unmarshal(bytes, &targets)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		group.Targets = targets
+	}
+
+	err = group.replaceVars(vars)
+	if err != nil {
+		return nil, err
+	}
+
+	return &group, nil
+}
+
 type Target struct {
-	Email     string `json:"email" yaml:"email"`
-	FirstName string `json:"first_name" yaml:"first-name"`
-	LastName  string `json:"last_name" yaml:"last-name"`
-	Position  string `json:"position" yaml:"position"`
+	FirstName string `json:"first_name" yaml:"first-name" csv:"First Name"`
+	LastName  string `json:"last_name" yaml:"last-name" csv:"Last Name"`
+	Email     string `json:"email" yaml:"email" csv:"Email"`
+	Position  string `json:"position" yaml:"position" csv:"Position"`
+}
+
+func (t *Target) replaceVars(vars map[string]string) error {
+	email, err := templateReplace(t.Email, vars)
+	if err != nil {
+		return err
+	}
+	t.Email = email
+
+	firstName, err := templateReplace(t.FirstName, vars)
+	if err != nil {
+		return err
+	}
+	t.FirstName = firstName
+
+	lastName, err := templateReplace(t.LastName, vars)
+	if err != nil {
+		return err
+	}
+	t.LastName = lastName
+
+	position, err := templateReplace(t.Position, vars)
+	if err != nil {
+		return err
+	}
+	t.Position = position
+
+	return nil
 }
 
 type GroupsSummary struct {
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/PuerkitoBio/goquery v1.8.1
 	github.com/go-resty/resty/v2 v2.7.0
+	github.com/gocarina/gocsv v0.0.0-20230406101422-6445c2b15027
 	gopkg.in/yaml.v2 v2.4.0
 )
 
